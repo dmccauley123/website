d4066b6d85856a0475ab407a20758762a9d3bfab
David McCauley <dmccauley@gmail.com>
2022-01-10 14:36:50 -0500

    spacing for contact

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -9,12 +9,12 @@ import Contact from "@lekoarts/gatsby-theme-cara/src/components/contact"
 
 const Cara = () => (
   <Layout>
-    <Parallax pages={6.8}>
+    <Parallax pages={7.3}>
       <Hero offset={0} factor={1} />
       <Gallery offset={1} factor={2} />
       <About offset={3} factor={2} />
       <Projects offset={4.3} factor={2} />
-      <Contact offset={5.8} factor={1} />
+      <Contact offset={6.3} factor={1} />
     </Parallax>
   </Layout>
 )
