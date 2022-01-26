4bf951481bc52c3ccca31efaf7554274ce3314f2
David McCauley <dmccauley@gmail.com>
2022-01-25 23:57:45 -0500

    center text headings

--- src/pages/mint.js
+++ src/pages/mint.js
@@ -22,13 +22,13 @@ const MintPage = () => (
        <div>
          <Divider speed={0.2} offset={0} factor={1}>
            <UpDown>
-              <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
-              <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
-              <Svg icon="box" width={6} color="icon_darker" left="60%" top="15%" />
+              <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" fill-opacity="0.4"/>
+              <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" fill-opacity="0.4"/>
+              <Svg icon="box" width={6} color="icon_darker" left="60%" top="15%" fill-opacity="0.4"/>
            </UpDown>
             <UpDownWide>
-              <Svg icon="arrowUp" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" />
-             <Svg icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" />
+              <Svg icon="arrowUp" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" fill-opacity="0.4" />
+             <Svg icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" fill-opacity="0.4" />
              <Svg icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
              <Svg icon="triangle" width={16} stroke color="icon_darkest" left="30%" top="65%" />
              <Svg icon="cross" width={16} stroke color="icon_pink" left="28%" top="15%" />
