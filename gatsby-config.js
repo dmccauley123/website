ee3d1d79d35bcb4ebafbc2885b60a9fb90e6439a
David McCauley <dmccauley@gmail.com>
2022-01-28 01:11:14 -0500

    update google ua tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          'G-PX2N7KR8QK', // Google Analytics / GA
+          'UA-218817666-1', // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
