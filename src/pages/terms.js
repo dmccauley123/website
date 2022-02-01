0b65f5b95921a34125973462bb198d28771bc55c
David McCauley <dmccauley@gmail.com>
2022-02-01 12:18:38 -0500

    removed style from legal for proper formatting

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -1,15 +1,10 @@
 import * as React from "react"
-import { Parallax } from "@react-spring/parallax"
-import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
 import Legal from "../@lekoarts/gatsby-theme-cara/components/legal"
 
 const Terms = () => (
-  <Layout>
-    <Parallax pages={3.5}>
-      <Legal offset={1} factor={0} />
 
-    </Parallax>
-  </Layout>
+      <Legal />
+
 )
 
 export default Terms
