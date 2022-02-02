0370c4b8c165b372a144abcc5239d9714a1ca84c
David McCauley <dmccauley@gmail.com>
2022-02-02 16:04:49 -0500

    Small updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,8 @@ module.exports = {
         openAnalyzer: false,
       },
     },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-react-helmet`,
     {
     resolve: 'gatsby-plugin-robots-txt',
       options: {
@@ -78,5 +80,20 @@ module.exports = {
       }
     },
     `gatsby-plugin-fontawesome-css`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        purgeCSSOptions: {
+          // https://purgecss.com/configuration.html#options
+          // safelist: ['safelist'], // Don't remove this selector
+        },
+        // More options defined here https://purgecss.com/configuration.html#options
+      },
+    },
   ].filter(Boolean),
 }
